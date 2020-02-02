23306662e31c0b3c932a75716b917eccb6cf480e
GRASS-120 <pokemon9116@yandex.ru>
2020-02-02 12:56:06 +0700

    add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
         return (
             <footer class="page-footer font-small blue pt-4">
                 <div class="footer-copyright text-center py-3">© 2020 Copyright:
-                    <a href="https://mdbootstrap.com/education/bootstrap/"> АЛЖИР</a>
+                    <a href="#">АЛЖИР</a>
                 </div>
             </footer>
         );
