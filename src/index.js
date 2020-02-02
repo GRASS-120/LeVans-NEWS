9c413dd30fc566a698a6e1d6fc3a42b4fc85af07
GRASS-120 <pokemon9116@yandex.ru>
2020-02-02 12:23:37 +0700

    v.0.0.2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let STATE = {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+}
+
+const Site = () => {
+    return (
+        <div id='page'>
+            <div id='page-wrap'>
+        
+            </div>
+            <footer>
+
+            </footer>
+        </div>
+    )
+}
+
+ReactDOM.render(<Site />, document.getElementById('root'));
 serviceWorker.unregister();
