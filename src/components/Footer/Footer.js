894b8ca13d7777e57acd96d0312716bb772c5e0c
GRASS-120 <pokemon9116@yandex.ru>
2020-02-02 13:11:28 +0700

    Update Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,7 @@ class Footer extends React.Component {
     render(){
         return (
             <footer class="page-footer font-small blue pt-4">
-                <div class="footer-copyright text-center py-3">© 2020 Copyright:
-                    <a href="#">АЛЖИР</a>
+                <div class="footer-copyright text-center py-3">© 2020 Copyright: АЛЖИР LeVans
                 </div>
             </footer>
         );
