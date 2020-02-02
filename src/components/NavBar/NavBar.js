af6e1400e69ed6b3364a4af04ab55b0af34496d7
Ivankub <vanadolgun@gmail.com>
2020-02-02 13:30:47 +0700

    Update NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -19,7 +19,7 @@ class NavBar extends React.Component {
                 </li>
                 <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
+                    Alexandr
                   </a>
                   <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="#">Action</a>
