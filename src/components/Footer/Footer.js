55f49462752f73014a62f9f5b2b1fc0016e56e30
GRASS_IT <45945921+GRASS-120@users.noreply.github.com>
2020-02-02 12:30:59 +0700

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render(){
+        return (
+            <footer class="page-footer font-small blue pt-4">
+                <div class="footer-copyright text-center py-3">© 2020 Copyright:
+                    <a href="https://mdbootstrap.com/education/bootstrap/"> АЛЖИР</a>
+                </div>
+            </footer>
+        );
+    }
+}
+export default Footer;
