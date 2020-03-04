fb8c716214bb912726bea2de9331935dfe7e3e98
Пользователь <vanadolgun@gmail.com>
2020-03-04 20:03:38 +0700

    navbar animation_2

--- maket/header/script.js
+++ maket/header/script.js
@@ -6,11 +6,69 @@ $(window).scroll(() => {
     if (scrollY >= sY/2) {
         $("#navbar").css("position", "fixed");
         $("#navbar").css("top","0px");
-        
     }
     else{
         $("#navbar").css("position", "absolute");
-        $("#navbar").css("top",(sY-scrollY)+"px");
+        $("#navbar").css({
+            "top":(sY-scrollY)+"px",
+            "height":"10.57em",
+            "transition": "0s",
+        });
+    }
+
+    if(scrollY >= sY/1.5){
+        $("#navbar").css({
+            "height":"6.5em",
+            "transition": "0.25s",
+        });
+        $("#aljir").css({
+            "width": "5em",
+            "margin-top": "0.6em",
+            "margin-left": "0.6em",
+        });
+        $(".block_navbar").css({
+            "top": "-1.51em",
+            "width": "6vw",
+        });
+        $("#icon").css({
+            "top": "0.2em",
+            "transition": "0.2s",
+        });
+        $("#search_icon").css({
+            "top": "0.2em",
+            "transition": "0.2s",
+        });
+        $("#search").css({
+            "top": "0.2em",
+            "transition": "0.2s",
+        });
+        setTimeout(() => {
+            $("#search_icon").css("transition", "1s");
+            $("#search").css("transition", "1s");
+        }, 200);
+    }else{
+        $("#navbar").css("height","10.57em");
+        $("#aljir").css({
+            "width": "7.55em",
+            "margin-top": "1.51em",
+            "margin-left": "2.57em",
+        });
+        $(".block_navbar").css({
+            "top": "-2.2em",
+            "width": "11.67vw",
+        });
+        $("#icon").css("top", "2.34em");
+        $("#search_icon").css({
+            "top": "2.34em",
+            "transition": "0.2s",
+        });
+        $("#search").css({
+            "top": "2.34em",
+            "transition": "0.2s",
+        });
+        setTimeout(() => {
+            $("#navbar").css("transition","0s");
+        }, 250);
     }
 });
 
