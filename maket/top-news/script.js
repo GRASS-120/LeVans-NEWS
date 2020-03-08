b566b84483a7666ff2196457baf0232dc21f3935
GRASS-120 <pokemon9116@yandex.ru>
2020-03-08 11:31:56 +0700

    создание слайдера в top news

--- maket/top-news/script.js
+++ maket/top-news/script.js
@@ -1,33 +1,33 @@
 
-$(document).ready(function(){
-    $('#next').click(function(){
-        let currentImage = $('.post.active')
-        let currentImageIndex = $('.post.active').index()
-        let nextImageIndex = currentImageIndex + 1
-        let nextImage = $('.post').eq(nextImageIndex)
+// $(document).ready(function(){
+//     $('#next').click(function(){
+//         let currentImage = $('.post.active')
+//         let currentImageIndex = $('.post.active').index()
+//         let nextImageIndex = currentImageIndex + 1
+//         let nextImage = $('.post').eq(nextImageIndex)
 
-        currentImage.fadeOut(1000);
-        currentImage.removeClass('active');
+//         currentImage.fadeOut(1000);
+//         currentImage.removeClass('active');
 
-        if(nextImageIndex == $('.post:last').index() + 1){
-            $('.post').eq(0).fadeIn(1000);
-            $('.post').eq(0).addClass('active');
-        } else {
-            nextImage.fadeIn(1000);
-            nextImage.addClass('active')
-        }
-    })
+//         if(nextImageIndex == $('.post:last').index() + 1){
+//             $('.post').eq(0).fadeIn(1000);
+//             $('.post').eq(0).addClass('active');
+//         } else {
+//             nextImage.fadeIn(1000);
+//             nextImage.addClass('active')
+//         }
+//     })
 
-    $('#prev').click(function(){
-        let currentImage = $('.post.active')
-        let currentImageIndex = $('.post.active').index()
-        let prevImageIndex = currentImageIndex - 1
-        let prevImage = $('.post').eq(prevImageIndex)
+//     $('#prev').click(function(){
+//         let currentImage = $('.post.active')
+//         let currentImageIndex = $('.post.active').index()
+//         let prevImageIndex = currentImageIndex - 1
+//         let prevImage = $('.post').eq(prevImageIndex)
 
-        currentImage.fadeOut(1000);
-        currentImage.removeClass('active');
-        prevImage.fadeIn(1000);
-        prevImage.addClass('active');
-    })
-})
+//         currentImage.fadeOut(1000);
+//         currentImage.removeClass('active');
+//         prevImage.fadeIn(1000);
+//         prevImage.addClass('active');
+//     })
+// })
 
