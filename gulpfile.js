ea9a0b1e6745fbd675fbf4a2555bdcd2482e8e0d
Lev Khomchenkov <pokemon9116@yandex.ru>
2020-03-12 23:25:16 +0700

    стили для news

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,25 @@ let gulp = require('gulp')
 let sass = require('gulp-sass')
 let browserSync = require('browser-sync')
 
-gulp.task('sass', function(){                                               // Создаем таск Sass
-    return gulp.src('./maket/sass/**/*.sass')                                   // Берем источник
-        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))   // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(gulp.dest('./maket/css'))                                         // Выгружаем результата в папку app/css
-        .pipe(browserSync.reload({stream: true}))                           // Обновляем CSS на странице при изменении
-});
-
-gulp.task('browser-sync', function() {                                      // Создаем таск browser-sync
-    browserSync({                                                           // Выполняем browserSync
-        server: {                                                           // Определяем параметры сервера
-            baseDir: './maket/news',                                                // Директория для сервера - app
+// сервер
+gulp.task('browser-sync', function() {    // Создаем таск browser-sync
+    browserSync({                         // Выполняем browserSync
+        server: {                         // Определяем параметры сервера
+            baseDir: './maket/news',      // Директория для сервера - app
         },
         notify: false,
         port: 4000,
         open: true,
-        notify: false                                                        // Отключаем уведомления
+        notify: false                     // Отключаем уведомления
     });
+});
+
+// обновление и компиляция sass -> css
+gulp.task('sass', function(){                                               // Создаем таск Sass
+    return gulp.src('./maket/sass/**/*.sass')                               // Берем источник
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))   // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('./maket/news'))                                     // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({stream: true}))                           // Обновляем CSS на странице при изменении
 });
 
 // обновление js
@@ -33,10 +35,12 @@ gulp.task('code', function() {
     .pipe(browserSync.reload({ stream: true }))
 });
 
+// слежение за изменениями в файлах
 gulp.task('watch', function() {
-    gulp.watch('./maket/sass/**/*.sass', gulp.parallel('sass'));                            // Наблюдение за sass файлами
-    gulp.watch('./maket/**/*.html', gulp.parallel('code'));                                    // Наблюдение за HTML файлами в корне проекта
-    gulp.watch('./maket/**/*.js', gulp.parallel('scripts'));     // Наблюдение за главным JS файлом и за библиотеками
+    gulp.watch('./maket/sass/**/*.sass', gulp.parallel('sass'));    // Наблюдение за sass файлами
+    gulp.watch('./maket/**/*.html', gulp.parallel('code'));         // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('./maket/**/*.js', gulp.parallel('scripts'));        // Наблюдение за главным JS файлом и за библиотеками
 });
 
+// сборочный таск
 gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
