7995d35a727dd4a43a54f210472fa94b92ea64b5
Пользователь <vanadolgun@gmail.com>
2020-03-22 11:32:50 +0700

    footer in app.js

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,1 +1,53 @@
-alert('hi');
+import React from 'react'
+import './Footer.css'
+
+class Footer extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            
+        }
+    }
+
+    render(){
+        return (
+            <div id="footer">
+                <div id="wrapper">
+                    <div id="content">
+                    </div>
+                    <footer>
+                        <div id="site_title">
+                            <p>LeVans © 2020</p>
+                            <hr/>
+                        </div>
+                        <div id="web_links">
+                            <div class="web_link">
+                                <a href="#">
+                                    <img src="./vk.png" alt=""/>
+                                </a>
+                            </div>
+                            <div class="web_link">
+                                <a href="#">
+                                    <img src="./telegram.png" alt=""/>
+                                </a>
+                            </div>
+                            <div class="web_link">
+                                <a href="#">
+                                    <img src="./reddi.png" alt=""/>
+                                </a>
+                            </div>
+                            <div class="web_link">
+                                <a href="#">
+                                    <img src="./facebook.png" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                    </footer>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Footer
