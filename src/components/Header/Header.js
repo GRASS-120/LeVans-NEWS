f3124472817c89154433431fd2e136cd69f2239a
Пользователь <vanadolgun@gmail.com>
2020-04-05 12:46:01 +0700

    header and navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,22 +10,27 @@ class NavBar extends React.Component {
         this.state = {
             sY: 312,
             search_pos: 0,
-            pos_y: 0
+            pos_y: 0,
+            burger: 0,
         }
     }
 
+
     componentDidMount(){
         window.addEventListener('scroll', this.handleScroll);
+        window.addEventListener('DOMContentLoaded', this.loader);
     }
-    
     componentWillUnmount(){
-        window.removeEventListener('scroll', this.handleScroll);
+        window.rvwoveEventListener('scroll', this.handleScroll);
     }
 
     
     handleScroll = (e)=>{
-        // let navbar = document.getElementById("navbar");
+        // let navbar = document.getElvwentById("navbar");
         let navbar = $("#navbar");
+        let sY_reserve = (parseInt($("#header-text").css("font-size"))*11);
+        console.log(sY_reserve)
+        this.state.sY = sY_reserve;
         // console.log(navbar.children());
         if (window.scrollY >= this.state.sY/2) {
             navbar.css("position", "fixed");
@@ -35,55 +40,60 @@ class NavBar extends React.Component {
             navbar.css("position", "absolute");
             navbar.css({
                 "top":(this.state.sY-window.scrollY)+"px",
-                "height":"10.57em",
+                "height":"8.57vw",
                 "transition": "0s",
             });
         }
     
         if(window.scrollY >= this.state.sY/1.5){
             navbar.css({
-                "height":"6.5em",
+                "height":"5vw",
                 "transition": "0.25s",
             });
-            $("#icons").css("right", "-3.9em");
+            $("#icons").css("right", "-3.9vw");
             $("#aljir").css({
-                "width": "5em",
-                "margin-top": "0.6em",
-                "margin-left": "0.6em",
+                "width": "4vw",
+                "margin-top": "0.5vw",
+                "margin-left": "0.5vw",
             });
+            // $("#nav_blocks").css({
+            //     "margin-top": "-3.3vw",
+            //     "margin-left": "4vw"
+            // });
             $(".block_navbar").css({
-                "top": "0.25em",
-                "width": "8vw",
+                "top": "0.25vw",
+                "width": "6.5vw",
             });
             $("#icon").css({
-                "top": "1.2em",
-                "right": "6.5em",
+                "top": "0.5vw",
+                "right": "6vw",
                 "transition": "0.2s",
-                "height": "3.5em",
-                "width": "3.5em",
+                "height": "3.5vw",
+                "width": "3.5vw",
             });
             $("#search_icon").css({
-                "top": "1.2em",
-                "right": "10.675em",
+                "top": "0.5vw",
+                "right": "10vw",
                 "transition": "0.2s",
-                "height": "3.5em",
-                "width": "3.5em",
+                "height": "3.5vw",
+                "width": "3.5vw",
                 "transition": "0.2s",
                 "transform": "rotate(0deg)",
             });
             $("#search").css({
-                "top": "1.2em",
-                "right": "10.675em",
+                "top": "0.5vw",
+                "right": "10vw",
                 "transition": "0.2s",
-                "height": "3.5em",
-                "width": "3.5em",
+                "height": "3.5vw",
+                "width": "3.5vw",
                 "transition": "0.2s",
             });
             $("#search_input").css({
                 "transition": "0.2s",
-                "height": "1.9em",
-                "padding-left": "0.03em",
-                "width": "0.1em",
+                "height": "3vw",
+                "padding-left": "0.02vw",
+                "width": "0.1vw",
+                "right": "0vw",
             });
             this.state.pos_y = 1;
             this.state.search_pos = 0;
@@ -91,45 +101,51 @@ class NavBar extends React.Component {
                 $("#search_icon").css("transition", "1s");
                 $("#search_input").css("transition", "1s");
                 $("#search").css("transition", "1s");
+                $("#search_input").css("display", "none");
             }, 200);
         }else{
-            navbar.css("height","10.57em");
-            $("#icons").css("right", "0em");
+            navbar.css("height","8.57vw");
+            $("#icons").css("right", "0vw");
             $("#aljir").css({
-                "width": "7.55em",
-                "margin-top": "1.51em",
-                "margin-left": "2.57em",
+                "width": "6vw",
+                "margin-top": "1.5vw",
+                "margin-left": "2vw",
             });
+            // $("#nav_blocks").css({
+            //     "margin-top": "-4.5vw",
+            //     "margin-left": "6.8vw"
+            // });
             $(".block_navbar").css({
-                "top": "0.5em",
-                "width": "11.67vw",
+                "top": "0.5vw",
+                "width": "11vw",
             });
             $("#icon").css({
-                "top": "2.34em",
-                "right": "2.45em",
-                "height": "5.89em",
-                "width": "5.89em",
+                "top": "2vw",
+                "right": "2vw",
+                "height": "4.8vw",
+                "width": "4.8vw",
             });
             $("#search_icon").css({
-                "top": "2.34em",
-                "right": "9.59em",
+                "top": "2vw",
+                "right": "8vw",
                 "transition": "0.2s",
-                "height": "5.89em",
-                "width": "5.89em",
+                "height": "4.8vw",
+                "width": "4.8vw",
                 "transform": "rotate(0deg)",
             });
             $("#search").css({
-                "top": "2.34em",
-                "right": "9.59em",
+                "top": "2vw",
+                "right": "8vw",
                 "transition": "0.2s",
-                "height": "5.89em",
-                "width": "5.89em",
+                "height": "4.8vw",
+                "width": "4.8vw",
             });
             $("#search_input").css({
                 "transition": "0.2s",
-                "height": "3.8em",
-                "padding-left": "0.1em",
-                "width": "0.3em",
+                "height": "4.3vw",
+                "padding-left": "0.1vw",
+                "width": "0.3vw",
+                "right": "0vw",
             });
             this.state.pos_y = 0;
             this.state.search_pos = 0;
@@ -138,45 +154,110 @@ class NavBar extends React.Component {
                 $("#search_icon").css("transition", "1s");
                 $("#search_input").css("transition", "1s");
                 $("#search").css("transition", "1s");
-            }, 250);
+                $("#search_input").css("display", "none");
+            }, 200);
         }
     }
 
     search_click(e) {
         if(this.state.pos_y == 0){
             if(this.state.search_pos == 0){
-                $("#search").css("width", "29.53em");
-                $("#search_icon").css("right", "33.23em").css("transform", "rotate(-360deg)");
-                $("#search_input").css("opacity", "1").css("width", "15.4em");
+                $("#search").css("width", "25vw");
+                $("#search_icon").css("right", "28.7vw").css("transform", "rotate(-360deg)");
+                $("#search_input").css("display", "block");
+                setTimeout(() => {
+                    $("#search_input").css("opacity", "1");
+                    $("#search_input").css("width", "20.5vw");
+                }, 1);
                 this.state.search_pos = 1;
             }else{
-                $("#search").css("width", "5.89em");
-                $("#search_icon").css("right", "9.59em");
+                $("#search").css("width", "4.8vw");
+                $("#search_icon").css("right", "8vw");
                 $("#search_icon").css("transform", "rotate(0deg)");
                 $("#search_input").css("opacity", "0");
-                $("#search_input").css("width", "4em");
+                $("#search_input").css("width", "4vw");
+                setTimeout(() => {
+                    $("#search_input").css("display", "none");
+                }, 1000);
                 this.state.search_pos = 0;
             }
         }else{
             if(this.state.search_pos == 0){
-                $("#search").css("width", "27.53em");
-                $("#search_icon").css("right", "34.92em");
+                $("#search").css("width", "23vw");
+                $("#search_icon").css("right", "30.39vw");
                 $("#search_icon").css("transform", "rotate(-360deg)");
-                $("#search_input").css("opacity", "1");
-                $("#search_input").css("width", "15.4em");
+                $("#search_input").css("display", "block");
+                setTimeout(() => {
+                    $("#search_input").css("opacity", "1");
+                    $("#search_input").css("width", "20vw");
+                }, 1);
                 this.state.search_pos = 1;
             }else{
-                $("#search").css("width", "3.5em");
-                $("#search_icon").css("right", "10.675em");
+                $("#search").css("width", "3.5vw");
+                $("#search_icon").css("right", "10.675vw");
                 $("#search_icon").css("transform", "rotate(0deg)");
                 $("#search_input").css("opacity", "0");
-                $("#search_input").css("width", "3em");
+                $("#search_input").css("width", "3vw");
+                setTimeout(() => {
+                    $("#search_input").css("display", "none");
+                }, 1000);
                 this.state.search_pos = 0;
             }
         }
     }
 
+    loader(e){
+        if(window.innerWidth <= window.innerHeight){
+            $(".block_navbar").css("font-size", "1.2em");
+            if(window.innerWidth <= window.innerHeight/2){
+                $(".block_navbar").css({
+                    "height": "2vw",
+                })
+                $("#nav_blocks").css({
+                    "width": "11vw",
+                    "height": "5vw",
+                    "opacity": "0",
+                    "margin-top": "-20vw",
+                    "top": "-15vw",
+                    "font-size": "1em",
+                });
+                $("#burger").css({
+                    "margin-left": "12vw",
+                    "display": "block",
+                });
+            }
+        }
+    }
 
+    burger(e){
+        if(this.state.burger == 0){
+            this.state.burger = 1;
+            $("#nav_blocks").css({
+                "opacity": "1",
+                "margin-top": "10vw",
+                "top": "0vw",
+            });
+            $("#burger").css({
+                "height": "35vw",
+                "width": "18vw",
+            });
+            $("#burger").css("margin-top", "-33vw");
+            $("#aljir").css("margin-top", "-51.5vw");
+        }else{
+            this.state.burger = 0;
+            $("#nav_blocks").css({
+                "opacity": "0",
+                "margin-top": "-20vw",
+                "top": "-15vw",
+            });
+            $("#burger").css({
+                "height": "8vw",
+                "width": "8vw",
+            });
+            $("#burger").css("margin-top", "-7vw");
+            $("#aljir").css("margin-top", "1.5vw");
+        }
+    }
 
     render(){
         return (
@@ -190,10 +271,20 @@ class NavBar extends React.Component {
                 </div>
                 <div id="navbar" className="container-fluid">
                     <img src="media\img\header\aljir.png" alt="" id="aljir"/>
-                    <div className="block_navbar"> #World</div>
-                    <div className="block_navbar"> #Tech</div>
-                    <div className="block_navbar"> #Games</div>
-                    <div className="block_navbar"> #Music</div>
+
+                    <div id="nav_blocks" className="container">
+                        <div className="block_navbar"> #World</div>
+                        <div className="block_navbar"> #Tech</div>
+                        <div className="block_navbar"> #Games</div>
+                        <div className="block_navbar"> #Music</div>
+                    </div>
+                    
+                    <div id="burger" className="container" onClick={this.burger.bind(this)}>
+                        <div id="one_burger" className="col"></div>
+                        <div id="two_burger" className="col"></div>
+                        <div id="three_burger" className="col"></div>
+                    </div>
+
                     <div id="icons" className="container">
                         <div id="icon"></div>
                         <img src="media\img\header\search.png" alt="" id="search_icon" onClick={this.search_click.bind(this)}/>
