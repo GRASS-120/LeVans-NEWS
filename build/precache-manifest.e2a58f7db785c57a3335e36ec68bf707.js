c24d4954aad218aad92817a8b9b834ec55323392
Lev Khomchenkov <pokemon9116@yandex.ru>
2020-04-12 11:06:14 +0700

    build

---
+++ build/precache-manifest.e2a58f7db785c57a3335e36ec68bf707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9709ac3dc4af7e0d1af4afc99526ee9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32785557c672879aa19a",
+    "url": "/static/css/main.7b664af4.chunk.css"
+  },
+  {
+    "revision": "e32c8d021a1c8d9484ff",
+    "url": "/static/js/2.50b5a760.chunk.js"
+  },
+  {
+    "revision": "35e9035c4541049d41232fe0bf96f70b",
+    "url": "/static/js/2.50b5a760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32785557c672879aa19a",
+    "url": "/static/js/main.a68fe7c4.chunk.js"
+  },
+  {
+    "revision": "58b3d8f183650da1394e",
+    "url": "/static/js/runtime-main.92dbaf88.js"
+  }
+]);
